39608dfb6abb3a9cb50bda80672f592d33c1e8aa
Sravanich123 <56504397+Sravanich123@users.noreply.github.com>
2022-04-03 18:30:12 +0530

    Update form1.php

--- form1.php
+++ form1.php
@@ -14,7 +14,7 @@ require 'config.php';
         // $target_file_path = $target_dir. $file_name;
         // move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file_path);
 
-        //$cloudinary->uploadApi()->text($file_name);
+        $cloudinary->uploadApi()->text($file_name);
 
         $arr= \Cloudinary\Uploader::upload($_FILES["file"]["tmp_name"], array("public_id" => $file_name ));
         
