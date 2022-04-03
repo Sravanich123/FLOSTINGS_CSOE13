24fd8fde85f97464fed06e43532328f5beac8cf9
Sravanich123 <56504397+Sravanich123@users.noreply.github.com>
2022-04-03 17:42:45 +0530

    Update search.php

--- search.php
+++ search.php
@@ -8,22 +8,22 @@
       while ($row = pg_fetch_row($result)) 
       { ?>
             <section class="record">
-            <!-- <div class="record-arrow">
+            <!--<div class="record-arrow">
                 <i style="color: rgb(21, 170, 46);" class="fa-solid fa-check"></i>
-            </div> -->
-            <div id="record1" class="record-display" >
-                <img src="<?php echo $row[4];?>" style="width:230px;height:290px;object-fit:fill;" alt="image"/>
-                <a href="#">CONTACT</a>
-             <div class="btn" style="display:flex; justify-content:space-around;">
+            </div>-->
+             <div id="record1" class="record-display" >
+            <img src="<?php echo $row[4];?>" style="width:230px;height:290px;object-fit:fill;" alt="image"/>
+            <div class="btn" style="display:flex; justify-content:space-around;">
                 <a href = "mailto:<?php echo $row[5];?>">CONTACT</a>
                 <form id="<?php echo $row[0];?>" action="del.php" method="post">
                     <input style="display:none;" type="number" name="id" value="<?php echo $row[0];?>">
                     <a onclick="del('<?php echo $row[5];?>',<?php echo $row[0];?>)"> <i title="Delete if the item was found" style="transform:none;" class="fa-solid fa-trash"></i></a>
                 </form>
+                
             </div>
-            </div>
+        </div>
             <div class="record-desc">
-              <div id="detail">
+            <div id="detail">
                 <h1>DETAILS</h1>
                 <p><b>Item: </b><?php echo $row[1];?><br>
                 <b>Found at: </b> <?php echo $row[2];?><br>
@@ -31,7 +31,7 @@
                 <b>Mob no: &nbsp;</b><?php echo $row[6];?><br>
                 </p>
             <div>
-            </div>
+        </div>
         </section>
      <?php }
     }
