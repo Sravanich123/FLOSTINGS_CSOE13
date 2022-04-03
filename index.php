b5671ab5fd5c635844cadd3a55dfb12812ce8cc9
Sravanich123 <56504397+Sravanich123@users.noreply.github.com>
2022-04-03 15:35:52 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Flostings</title>
+    <link rel="shortcut icon" href="src/f.png">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=PT+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="sample.css">
+    <script src="https://kit.fontawesome.com/57630a8715.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <ul>
+        <li><a href="form.php" style="text-decoration: none;">Form Page <i class="fa-solid fa-angle-right"></i></a></li>
+        <li><a href="index1.php" style="text-decoration: none;">View Items <i class="fa-solid fa-angle-right"></i></a></li>
+    </ul>
+    <div class="hero_image">
+        <div class="hero_text">
+            <h1>FLOSTINGS</h1>
+            <a id="subhead">Found or Lost Things</a><br>
+            <button onclick="window.location.href='form.php'">Found Something?</button>
+            <button onclick="window.location.href='index1.php'">Lost Something?</button></div>
+    </div>
+</body>
+</html>
