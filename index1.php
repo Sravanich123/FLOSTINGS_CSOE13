0562ad805888fd3e437ec52062a2080e5d5686c1
Sravanich123 <56504397+Sravanich123@users.noreply.github.com>
2022-04-03 16:25:52 +0530

    Create index1.php

---
+++ index1.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <link href="https://fonts.googleapis.com/css2?family=PT+Sans&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/8f186daecf.js" crossorigin="anonymous"></script>
+    <link rel="shortcut icon" href="src/f.png">
+    <title>Flostings_Items Page</title>
+    <link rel="stylesheet" href="admin_page.css">
+</head>
+
+<body> 
+    <ul>
+        <li><a href="index.php"><i class="fa-solid fa-house">&nbsp;</i>Home</a></li>
+        <li><a href="form.html">Form page <i class="fa-solid fa-angle-right icon"></i></a></li>
+   </ul>
+  <form>
+    <div id="search">
+        <abbr title="Ctrl+Enter to search">
+        <input type="search" id="search_text" placeholder="Search for an item">
+        </abbr>
+        <a href="index1.php"><i onclick="search()" class="fa-solid fa-arrow-rotate-right"></i></a>
+    </div>
+  </form>   
+
+<div class="event-card--conatiner" id="output">
+
+    <?php
+         include 'db.php';
+    $query = "SELECT * FROM Images ORDER BY Found_on DESC";
+    $result = pg_query($con, $query); 
+    if (pg_num_rows($result)>0)
+    {
+      while ($row = pg_fetch_row($result)) {   
+    ?>
+
+    <section class="record">
+        <!-- <div class="record-arrow">
+            <i style="color: rgb(21, 170, 46);" class="fa-solid fa-check"></i>
+        </div> -->
+        <div id="record1" class="record-display" >
+            <img src="<?php echo 'Images/'.$row[4];?>" style="width:230px;height:290px;object-fit:fill;" alt="image"/>
+            <div class="btn" style="display:flex; justify-content:space-around;">
+                <a href = "mailto:<?php echo $row[5];?>">CONTACT</a>
+                <form id="<?php echo $row[0];?>" action="del.php" method="post">
+                    <input style="display:none;" type="number" name="id" value="<?php echo $row[0];?>">
+                    <a onclick="del('<?php echo $row[5];?>',<?php echo $row[0];?>)"> <i title="Delete if the item was found" style="transform:none;" class="fa-solid fa-trash"></i></a>
+                </form>
+                
+            </div>
+        </div>
+        <div class="record-desc">
+            <div id="detail">
+                <h1>DETAILS</h1>
+                <p><b>Item: </b><?php echo $row[1];?><br>
+                <b>Found at: </b> <?php echo $row[2];?><br>
+                <b>Found on: </b><?php echo $row[3];?><br>
+                <b>Mob no: &nbsp;</b><?php echo $row[6];?><br>
+                </p>
+            <div>
+        </div>
+    </section>
+
+      <?php }}?>
+</div>
+
+<script type="text/javascript">
+function del(x,y)
+{  
+    var email_id=prompt("Enter your Email id (of uploader):");
+    if(x==email_id)
+    {   
+        document.getElementById(y).submit();  
+    }
+    else if(email_id && email_id!=x && (email_id.includes("@")))
+    {           
+        alert("You don't have access to delete this item. Only the uploader can delete.");
+    }
+    else if(email_id)
+    {
+        alert("Please enter a valid email address!")
+    }
+    
+}
+  $(document).ready(function(){
+      $("#search_text").keypress(function(){
+          $.ajax({
+              type:'POST',
+              url:'search.php',
+              data:{
+                  name:$("#search_text").val(),
+              },
+              success:function(data){
+                $("#output").html(data);
+              }
+          });
+      });
+  });
+</script>
+    
+</body>
+</html>
+
+
+
+
+
