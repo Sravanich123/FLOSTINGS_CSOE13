259c2ea1cb669da2dad35953a45b84b557ef2f16
Sravanich123 <56504397+Sravanich123@users.noreply.github.com>
2022-04-03 17:41:10 +0530

    Update search.php

--- search.php
+++ search.php
@@ -14,17 +14,23 @@
             <div id="record1" class="record-display" >
                 <img src="<?php echo $row[4];?>" style="width:230px;height:290px;object-fit:fill;" alt="image"/>
                 <a href="#">CONTACT</a>
+             <div class="btn" style="display:flex; justify-content:space-around;">
+                <a href = "mailto:<?php echo $row[5];?>">CONTACT</a>
+                <form id="<?php echo $row[0];?>" action="del.php" method="post">
+                    <input style="display:none;" type="number" name="id" value="<?php echo $row[0];?>">
+                    <a onclick="del('<?php echo $row[5];?>',<?php echo $row[0];?>)"> <i title="Delete if the item was found" style="transform:none;" class="fa-solid fa-trash"></i></a>
+                </form>
+            </div>
             </div>
             <div class="record-desc">
+              <div id="detail">
                 <h1>DETAILS</h1>
                 <p><b>Item: </b><?php echo $row[1];?><br>
-                <b>Found at: &nbsp;</b> <?php echo $row[2];?><br>
+                <b>Found at: </b> <?php echo $row[2];?><br>
                 <b>Found on: </b><?php echo $row[3];?><br>
-                <b>Mob no: &nbsp;&nbsp;&nbsp;</b><?php echo $row[6];?><br>
+                <b>Mob no: &nbsp;</b><?php echo $row[6];?><br>
                 </p>
-                <!-- <button class="contact-button">
-                <a href="#">CONTACT</a>
-                </button> -->
+            <div>
             </div>
         </section>
      <?php }
